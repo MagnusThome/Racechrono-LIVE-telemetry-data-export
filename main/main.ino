482a446e5cbf7f557fcc12681f89174f73ad6dd9
Magnus Thomé <32169384+MagnusThome@users.noreply.github.com>
2022-12-31 11:22:23 +0100

    Removed local display and added remote update over internet instead

--- main/main.ino
+++ main/main.ino
@@ -1,204 +1,66 @@
 #include <Arduino.h>
-#include <SPI.h>
-#include "Adafruit_GFX.h"
-#include "Adafruit_ILI9341.h"     // https://github.com/adafruit/Adafruit_ILI9341
 #include "rcmonitor.h"            // https://github.com/aollin/racechrono-ble-diy-device
-
-//#define DUMMYTEST   // For testing, fetch magnetometer data instead of time and speed gain
-
-
-// ESP32 SPI: 
-// MISO  GPIO19
-// MOSI  GPIO23
-// CLK   GPIO18
-// SS    GPIO5
-#define TFT_CS 5
-#define TFT_DS 22
-#define TFT_RST 21
-
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DS, TFT_RST); 
+#include <WiFi.h>
+#include <HTTPClient.h>
 
 
-// Data to fecth from Racechrono
+
+const char* ssid = "xxxxxxxxxx";
+const char* password = "xxxxxxxxxxxxxxx";
+
+
 struct strmon monitors[] = {
-#ifdef DUMMYTEST
-    {"x_magnetic_field", "channel(device(magn), x_magnetic_field)", 1.0},
-    {"y_magnetic_field", "channel(device(magn), y_magnetic_field)", 1.0}
-#else
-    {"Time Delta",        "channel(device(lap), delta_lap_time)*10.0", 0.1},
-    {"Speed Delta",       "channel(device(gps), delta_speed)*10.0", 0.1}
-#endif
+    {"engine_oil_temp", "channel(device(obd), engine_oil_temp)", 1.0},
+    {"coolant_temp", "channel(device(obd), coolant_temp)", 1.0},
 };
 
-
-#define DIGITSIZE 7
-uint16_t totwidth;
-uint16_t totheight;
-
-
-void timegainPrint(float gain);
-void speedgainPrint(int16_t gain);
-void printFloat(float gain, uint16_t txcolor, uint16_t bgcolor);
-void printInt(uint16_t gain, uint16_t txcolor, uint16_t bgcolor);
-void initscreen(void);
-void bootscreensequence(void);
-
+#define BUFFSIZE 500
+char buff[BUFFSIZE];
+HTTPClient http;
 
 
 void setup() {
-  Serial.begin(115200);
-  delay(200);
-  Serial.println("\nBooting...");
-  initscreen();
-  bootscreensequence();
+  Serial.begin(115200);   
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) { delay(500); }
   rcmonitorstart();
 }
 
 
 void loop() {
-  static float timegain;
-  static int16_t speedgain;
+  static float engine_oil_temp;
+  static float coolant_temp;
   if( rcmonitor() ) {
-    timegain = monitorValues[0] * monitorMultipliers[0];
-    speedgain = (int16_t) (monitorValues[1] * monitorMultipliers[1]);
-    timegainPrint(timegain);
-    speedgainPrint(speedgain);
+    engine_oil_temp   = (monitorValues[0] * monitorMultipliers[0]);
+    coolant_temp      = (monitorValues[1] * monitorMultipliers[1]);
+    snprintf( buff, BUFFSIZE, "http://www.mywebserversomewhere.com/somepath/update.php?engine_oil_temp=%f&coolant_temp=%f", engine_oil_temp, coolant_temp ); 
+    http.begin(buff); 
+    http.GET();
+    http.end();
+    Serial.println(buff);   
   }
-  delay(900);
+  delay(2000);
 }
 
 
-void timegainPrint(float gain) {
-    static uint16_t txcolor;
-    static uint16_t bgcolor;
-    if(gain>=0) { 
-        txcolor = ILI9341_WHITE;
-        bgcolor = ILI9341_GREEN; 
+/*
+
+The webpage to send updates to:
+
+<?php
+  if(!empty($_GET["engine_oil_temp"]) && !empty($_GET["coolant_temp"])) {
+    $engine_oil_temp = preg_replace('/[^0-9.]/', '', $_GET['engine_oil_temp']);
+    $coolant_temp = preg_replace('/[^0-9.]/', '', $_GET['coolant_temp']);
+    $csvData = array(date("Ymd H:i:s"), $engine_oil_temp, $coolant_temp);
+    $fp = fopen("datalog.csv","a"); 
+    if($fp)    {  
+        fputcsv($fp,$csvData); 
+        fclose($fp); 
     }
-    else if(gain>-2) { 
-        txcolor = ILI9341_RED;
-        bgcolor = ILI9341_BLACK; 
-    } 
-    else { 
-        txcolor = ILI9341_WHITE;
-        bgcolor = ILI9341_RED; 
-    }
-    printFloat(gain, txcolor, bgcolor);
-}
+  }
+?>
 
+Then add a web page that reads the datalog file and displays it
 
-void speedgainPrint(int16_t gain) {
-    static uint16_t txcolor;
-    static uint16_t bgcolor;
-    if(gain>=0) { 
-        txcolor = ILI9341_WHITE;
-        bgcolor = ILI9341_GREEN; 
-    }
-    else if(gain>-10) { 
-        txcolor = ILI9341_RED;
-        bgcolor = ILI9341_BLACK; 
-    } 
-    else { 
-        txcolor = ILI9341_WHITE;
-        bgcolor = ILI9341_RED; 
-    }
-    printInt(gain, txcolor, bgcolor);
-}
-
-
-void printFloat(float gain, uint16_t txcolor, uint16_t bgcolor) {
-    const uint8_t XPOS = 34;
-    const uint8_t YPOS = 38;
-    static uint16_t oldcolor;
-    Serial.println(gain, 1);
-    tft.setTextSize(DIGITSIZE);
-    tft.setTextColor(txcolor, bgcolor);  
-    if(oldcolor != bgcolor) {
-      tft.fillRect(0, 0, totwidth, (totheight/2)-1, bgcolor);
-      yield();
-      oldcolor = bgcolor;
-    }
-    tft.setCursor(XPOS, YPOS);
-    if ( gain <= -100 || gain >= 1000  ) {
-      tft.print("over  ");
-      return;
-    }
-    tft.printf("%5.1f",gain);
-    tft.fillRect(XPOS+126, YPOS+48, 30, -30, bgcolor);    // erase dot
-    tft.fillRect(XPOS+126, YPOS+48, 8, -8, txcolor);      // create new dot
-    tft.setCursor(XPOS+143, YPOS);                        // prepare to overwrite decimal digit
-    tft.print((int16_t)abs(((gain-(int16_t)gain)*10)));   // overwrite decimal digit with same number but slightly to the left
-    tft.print(" ");                                       // clear any leftover parts of old decimal digit 
-}                                                         // All this to get a tighter kerning between the numbers on screen. Looks better :-)
-
-
-void printInt(int16_t gain, uint16_t txcolor, uint16_t bgcolor) {
-    const uint8_t XPOS = 38;
-    const uint8_t YPOS = 155;
-    static uint16_t oldcolor;
-    Serial.println(gain);
-    tft.setTextSize(DIGITSIZE);
-    tft.setTextColor(txcolor, bgcolor);  
-    if(oldcolor != bgcolor) {
-      tft.fillRect(0, (totheight/2)+1, totwidth, totheight, bgcolor);
-      yield();
-      oldcolor = bgcolor;
-    }
-    tft.setCursor(XPOS, YPOS);
-    if ( gain <= -100 || gain >= 1000  ) {
-      tft.print("over  ");
-      return;
-    }
-    tft.printf("%4d", gain);
-}
-
-
-void initscreen(void) {
-  tft.begin();
-  tft.setRotation(3);
-  totwidth = tft.width();
-  totheight = tft.height();
-  Serial.print("totwidth: ");
-  Serial.println(totwidth);
-  Serial.print("totheight: ");
-  Serial.println(totheight);
-}
-
-
-void bootscreensequence(void) {
-  tft.setTextSize(2);
-  tft.fillScreen(ILI9341_BLACK);
-  tft.setTextColor(ILI9341_WHITE);  
-  tft.setCursor( 103, 90 );
-  tft.println("Racechrono");
-  tft.setCursor( 86, 130 );
-  tft.println("with RejsaCAN");
-  delay(3000);
-  tft.fillScreen(ILI9341_WHITE);
-  tft.fillRect(0, 0, totwidth, (totheight/2)-1, ILI9341_BLACK);
-  tft.fillRect(0, (totheight/2), totwidth, totheight, ILI9341_BLACK);
-  tft.setTextSize(4);
-  tft.setTextColor(ILI9341_RED);  
-  tft.setCursor( 52, 45 );
-  tft.println("TIME GAIN");
-  tft.setCursor( 40, 160 );
-  tft.println("SPEED GAIN");
-  delay(1600);
-  tft.setTextColor(ILI9341_GREEN);  
-  tft.setCursor( 40, 160 );
-  tft.println("SPEED GAIN");
-  delay(1200);
-  tft.setCursor( 52, 45 );
-  tft.println("TIME GAIN");
-  delay(2300);
-  tft.fillScreen(ILI9341_BLACK);
-  tft.setTextColor(ILI9341_RED);  
-  tft.setTextSize(2);
-  tft.setCursor( 26, 90 );
-  tft.println("Waiting for connection");
-  tft.setCursor( 60, 130 );
-  tft.println("from Racechrono");
-}
-
-
-// end
+*/
